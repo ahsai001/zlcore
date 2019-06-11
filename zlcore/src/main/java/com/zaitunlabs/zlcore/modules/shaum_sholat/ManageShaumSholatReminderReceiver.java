a22e8b38426705da51957be6bfcbc372539265c8
Ahmad Saifullah <ahsai001@gmail.com>
2019-06-11 22:41:24 +0700

    update remider sholat

--- zlcore/src/main/java/com/zaitunlabs/zlcore/modules/shaum_sholat/ManageShaumSholatReminderReceiver.java
+++ zlcore/src/main/java/com/zaitunlabs/zlcore/modules/shaum_sholat/ManageShaumSholatReminderReceiver.java
@@ -16,8 +16,12 @@ import androidx.core.app.JobIntentService;
 public class ManageShaumSholatReminderReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(intent.getAction() != null && (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
-                intent.getAction().startsWith(ZLCoreConstanta.ACTION_MANAGE_SHAUM_SHOLAT_REMINDER))) {
+        if(intent.getAction() != null && (
+                intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
+                intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED) ||
+                intent.getAction().equals(Intent.ACTION_REBOOT) ||
+                intent.getAction().equals("android.intent.action.QUICKBOOT_POWERON") ||
+                intent.getAction().startsWith(ZLCoreConstanta.ACTION_MANAGE_SHAUM_SHOLAT_REMINDER) )) {
             JobIntentService.enqueueWork(context, ShaumSholatReminderService.class, ShaumSholatReminderService.JOB_ID, new Intent());
         }
     }
--- zlcore/src/main/java/com/zaitunlabs/zlcore/services/DataIntentService.java
+++ zlcore/src/main/java/com/zaitunlabs/zlcore/services/DataIntentService.java
@@ -1,6 +1,5 @@
 package com.zaitunlabs.zlcore.services;
 
-import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
 
